ba2e7b1cc4bd66cb6cc82e84526f08318cf30cf1
karthik <karthik.vbdon88@gmail.com>
2023-05-07 13:09:37 +1200

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//create a variable which takes a string
+let paragraphElement = document.querySelector("#paragraph");
+let btnCount = document.querySelector(".btn-count");
+let display = document.querySelector(".display");
+
+//write a function to output of words
+function countOccurences(para) {
+  let words = para.split(" ");
+  let wordCount = new Map();
+
+  words.forEach((element) => {
+    if (wordCount.has(element)) {
+      wordCount.set(element, wordCount.get(element) + 1);
+    } else {
+      wordCount.set(element, 1);
+    }
+  });
+  wordCount.forEach((word, count) => {
+    display.innerHTML += `${word}=> ${count}<br>`;
+  });
+
+  console.log(wordCount);
+  return wordCount;
+}
+
+btnCount.addEventListener("click", () => {
+  countOccurences(paragraphElement.value);
+});
